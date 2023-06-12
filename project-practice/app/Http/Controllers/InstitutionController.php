042887c14582820d8b1da71aa074767cf84cafa8
Kelvin Rivas <kelvin.arnoldo@gmail.com>
2023-06-11 23:04:18 -0600

    Agregando Funciones al controlador

--- project-practice/app/Http/Controllers/InstitutionController.php
+++ project-practice/app/Http/Controllers/InstitutionController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Institution;
 use Illuminate\Http\Request;
 
 class InstitutionController extends Controller
 {
-    //
+    public function index()
+    {
+        $instituciones = Institution::all();
+        return view('Institution.index', compact('Institution'));
+    }
+
+    public function create()
+    {
+        return view('Institution.create');
+    }
+
+    public function store(Request $request)
+    {
+        $institucion = Institution::create($request->all());
+        return redirect()->route('Institution.index');
+    }
+
+    public function show(Institution $institucion)
+    {
+        return view('Institution.show', compact('Institution'));
+    }
+
+    public function edit(Institucion $institucion)
+    {
+        return view('Institution.edit', compact('Institution'));
+    }
+
+    public function update(Request $request, Institution $institucion)
+    {
+        $institucion->update($request->all());
+        return redirect()->route('Institution.index');
+    }
+
+    public function destroy(Institution $institucion)
+    {
+        $institucion->delete();
+        return redirect()->route('Institution.index');
+    }
 }
--- project-practice/app/Http/Controllers/ProjectController.php
+++ project-practice/app/Http/Controllers/ProjectController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(): Renderable
+    {
+        $projects = Project::paginate(1);
+        return view('projects.index', compact('projects'));
+    }
 }
